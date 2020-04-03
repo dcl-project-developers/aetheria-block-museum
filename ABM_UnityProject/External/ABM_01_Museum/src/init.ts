c71a1331a70740832d18252004ca2124a4c5d5f9
ai47patos <ai47patos@gmail.com>
2020-04-03 17:42:39 +0200

    signpannel

--- ABM_UnityProject/External/ABM_01_Museum/src/init.ts
+++ ABM_UnityProject/External/ABM_01_Museum/src/init.ts
@@ -1,4 +1,7 @@
 import { TagComponent, MovementType, FollowPathMoveComponent, PathFollower, Path, UpdateMovementComponentsSystem } from "./imports/index"
+import { SignPanel,SignBook } from "./signBook"
+import { getUserData } from '@decentraland/Identity'
+import { getUserAccount } from '@decentraland/EthereumController'
 
 engine.addSystem(new UpdateMovementComponentsSystem())
 
@@ -30,6 +33,60 @@ function loadPathFollowers() {
     }
   }
 }
+
+/*
+*   Load Sign Panel
+*/
+function loadSignPanel(address:string, name:string) {
+  var signPanelsArray: SignPanel[] = []
+  for (const entityId in engine.getEntitiesWithComponent(TagComponent)) {
+    let entity: IEntity = engine.getEntitiesWithComponent(TagComponent)[entityId]
+    if (entity.getComponent(TagComponent).tag=="signpannel") {
+      let panelInfo = {
+        board: null, next: null, back: null, pages: null
+      }
+      for (const childid in entity.children) {
+        let child = entity.children[childid]
+        if (child.hasComponent(TagComponent)) {
+          if (child.getComponent(TagComponent).tag=="signpannel_board") {
+            panelInfo.board = child
+          }
+          else if (child.getComponent(TagComponent).tag=="signpannel_next") {
+            panelInfo.next = child
+          }
+          else if (child.getComponent(TagComponent).tag=="signpannel_back") {
+            panelInfo.back = child
+          }
+          else if (child.getComponent(TagComponent).tag=="signpannel_pages") {
+            panelInfo.pages = child
+          }
+        }
+      }
+      if (panelInfo.board && panelInfo.next && panelInfo.back && panelInfo.pages) {
+        var pannel = new SignPanel(entity, panelInfo.board, panelInfo.next, panelInfo.back, panelInfo.pages)
+        entity.addComponent(pannel)
+        signPanelsArray.push(pannel)
+      }
+    }
+  }
+  for (const entityId in engine.getEntitiesWithComponent(TagComponent)) {
+    let entity: IEntity = engine.getEntitiesWithComponent(TagComponent)[entityId]
+    if (entity.getComponent(TagComponent).tag=="signbook") {
+        entity.addComponent(new SignBook(entity, address, name, signPanelsArray))
+    }
+  }
+}
+
+//Load user name/id
+executeTask(async () => {
+    try {
+      const userData = await getUserData()
+      const address = await getUserAccount()
+      loadSignPanel(address, userData.displayName)
+    } catch (error) {
+      log(error.toString())
+    }
+})
 
 export const loadInit = function(){
   loadPath()
